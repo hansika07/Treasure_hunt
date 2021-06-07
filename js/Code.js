811c5fa1e09d0bb2e295d925746ce34000af4a5a
hansika07 <77222579+hansika07@users.noreply.github.com>
2021-06-07 12:13:25 +0530

    treasure hunt
    treasure hunt

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+fill("white")
+    textSize(15)
+    text("T U N C F O I N", 700,150)
+    fill("lightGreen")
+    text("Hint: Performs a perticular task !!", 700,170)    
+
+    fill("white")
+    textSize(15)
+    text(" A T A D A S E B", 100,290)
+    fill("lightGreen")
+    text("Hint: A THING THAT STORES ALL THE THING IN PROGRAMMING !!", 100,320)    
+
+
+}
